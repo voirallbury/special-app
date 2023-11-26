46601bdac697b19514dbddc4b73f53218b041baf
Goncharov Aleksei <allburypirog@gmail.com>
2023-11-26 12:19:22 +0300

    clear form deleted from main.js

--- js/main.js
+++ js/main.js
@@ -8,10 +8,6 @@ $(function(){
         $('.popup-container').css('display', 'none');
     };
 
-    let clearForm = function(){
-        $('.popup-container > input[type="text"]').val('');
-    };
-
     $('.close').on('click', function(){
         clearForm();
         closePopup();
