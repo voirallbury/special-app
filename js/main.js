9f06d76b40a27363f6ee8fe6133728f988db3a61
Goncharov Aleksei <allburypirog@gmail.com>
2023-11-26 12:49:23 +0300

    2 commits back

--- js/main.js
+++ js/main.js
@@ -8,6 +8,10 @@ $(function(){
         $('.popup-container').css('display', 'none');
     };
 
+    let clearForm = function(){
+        $('.popup-container > input[type="text"]').val('');
+    };
+
     $('.close').on('click', function(){
         clearForm();
         closePopup();
